b77ee188d72622c5a6cca244e74a5bb480922d8e
xrehub <hubert.kielbasinski99@wp.pl>
2025-11-13 20:02:20 +0100

    modified setup.sh

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -13,3 +13,17 @@ for i in {1..5}; do
   echo "log entry $1" > "${TEST_DIR0}/app.log.$i"
   touch -d "$i days ago" "${TEST_DIR}/app.log.$i"
 done
+
+cat > "$CONF" <<EOF
+LOG_SOURCES="${TEST_DIR}"
+ARCCHIVE_DIR="${ARCHIVE}"
+DAYS_TO_ARCHIVE=2
+RETENTION_DAYS=7
+DELETE_ORIGINAL="no"
+ALERT_EMAIL=""
+MAX_ARCHIVE_SIZE_MB=0
+EOF
+
+
+echo "Test logs and configuration ceated"
+echo "Run test: ..asal.sh ./tests/asal_test.conf"
